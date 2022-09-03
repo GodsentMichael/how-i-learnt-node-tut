c67231f64f8c12206dcb7bb42e1873d30f473419
Godsent Michael <gentlemotivator@gmail.com>
2022-09-03 01:16:06 +0100

    continuation of my tutorial

--- app.js
+++ app.js
@@ -14,8 +14,67 @@
 // 2. npm init (step by step, press enter to skip)
 // 3. npm init -y (everything default)
 
-const _ = require("lodash");
+// const _ = require("lodash");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
+// const items = [1, [2, [3, [4]]]];
+// const newItems = _.flattenDeep(items);
+// console.log(newItems);
+
+const express = require("express");
+const fsPromises = require("fs").promises;
+
+const PORT = 8000;
+const app = express();
+
+app.use(express.static("content"));
+app.use(express.json);
+
+//Synchronous error is automatically handled by express
+app.get("/", (req, res) => {
+    throw new Error("Hello error!");
+});
+
+//Asynchronous error is not handled by express
+app.get("/file", async(req, res) => {
+    const file = await fsPromises.readFile("./no-such-file.txt");
+    res.sendFile(file);
+});
+
+app.get("/text", async(req, res, next) => {
+    try {
+        const file = await fsPromises.readFile("./no-such-file.txt");
+        res.sendFile(file);
+    } catch (error) {
+        error.type = "Not Found";
+        next(error);
+    }
+});
+
+app.get("/user", async(req, res, next) => {
+    try {
+        const file = await fsPromises.readFile("./no-such-file.txt");
+        res.sendFile(file);
+    } catch (error) {
+        error.type = "Redirect";
+        next(error);
+    }
+});
+
+//Handle asynchronous error using error middleware
+app.use((error, req, res, next) => {
+    console.log("Error Handling Middleware called");
+    console.log("Path:", req.path);
+    console.error("Error:", error);
+
+    if (error.type == "Redirect") res.redirect("error.html");
+    else if (error.type == "Not Found")
+    //arbitrary condition check
+        res.status(500).send("file not found!");
+    else {
+        res.status(404).send(error);
+    }
+    next();
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
